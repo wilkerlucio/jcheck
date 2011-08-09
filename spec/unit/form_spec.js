330a6afcbf3832206e9275adb7f9be78ff9080ff
Wilker Lucio <wilkerlucio@gmail.com>
2011-08-09 14:03:57 -0300

    enabling / disabling jcheck

--- spec/unit/form_spec.js
+++ spec/unit/form_spec.js
@@ -1,188 +1,210 @@
 describe "jCheck"
-	describe "FormCheck"
-		describe "validating"
-			before_each
-				v = $(fixture("form")).jcheck()
-				v.validate(function(f) {})
-			end
-			
-			it "should don't be valid if has errors"
-				v.validate(function(f) {f.errors.add("text", "some")})
-				v.is_valid().should.be_false 
-			end
-			
-			it "should be valid if has no errors"
-				v.is_valid().should.be_true 
-			end
-		end
-		
-		describe "notifiers setup"
-			it "should user notification_dialog and tip_ballons as default notifiers"
-				v = $(fixture("form")).jcheck()
-				v.should.have 1, 'notifiers'
-				v.should.have 1, 'live_notifiers'
-				v.notifiers[0].should.be_an $.FormCheck.Notifiers.NotificationDialog
-				v.live_notifiers[0].should.be_an $.FormCheck.Notifiers.TipBalloons
-			end
-			
-			it "should be able to customize overall notifiers on creation"
-				v = $(fixture("form")).jcheck({notifiers: []})
-				v.should.have 0, 'notifiers'
-				v.should.have 1, 'live_notifiers'
-				v.live_notifiers[0].should.be_an $.FormCheck.Notifiers.TipBalloons
-			end
-			
-			it "should be able to customize live notifiers on creation"
-				v = $(fixture("form")).jcheck({live_notifiers: []})
-				v.should.have 1, 'notifiers'
-				v.should.have 0, 'live_notifiers'
-				v.notifiers[0].should.be_an $.FormCheck.Notifiers.NotificationDialog
-			end
-			
-			it "should be able to customize both kind notifiers at once"
-				v = $(fixture("form")).jcheck({notifiers: [], live_notifiers: []})
-				v.should.have 0, 'notifiers'
-				v.should.have 0, 'live_notifiers'
-			end
-			
-			it "should be able to customize default overall notifiers"
-				jQuery.FormCheck.default_notifiers = []
-				
-				v = $(fixture("form")).jcheck()
-				v.should.have 0, 'notifiers'
-				
-				jQuery.FormCheck.default_notifiers = ["notification_dialog"]
-			end
-			
-			it "should be able to customize default live notifiers"
-				jQuery.FormCheck.default_live_notifiers = []
-				
-				v = $(fixture("form")).jcheck()
-				v.should.have 0, 'live_notifiers'
-				
-				jQuery.FormCheck.default_live_notifiers = ["tip_balloons"]
-			end
-		end
-		
-		describe "Field"
-			describe "labels"
-				before_each
-					v = $(fixture("form")).jcheck()
-				end
+  describe "FormCheck"
+    describe "disabling jcheck"
+      before_each
+        form = $(fixture("form"))
+        form.submit(function(e) {e.preventDefault()})
+        v = form.jcheck()
+        v.disable()
+      end
 
-				it "should get label if it's present"
-					v.field("text").label().should.eql "TextField"
-				end
+      it "should not run jcheck if it is disabled"
+        v.should_not.receive("is_valid")
 
-				it "should return field name if label is not present"
-					v.field("multiple_select").label().should.eql "multiple_select"
-				end
+        form.submit()
+      end
 
-				it "should get label for multiple fields"
-					v.field("checkboxes[]").label().should.eql "Check List"
-				end
+      it "should run jcheck if it is re-enabled"
+        v.enable()
+        v.should.receive("is_valid")
 
-				it "should able to use custom label"
-					v.field("text").custom_label = "Custom Label"
-					v.field("text").label().should.eql "Custom Label"
-				end
-			end
+        form.submit()
+      end
+    end
 
-			describe "field_prefix"
-				before_each
-					v = $(fixture("form")).jcheck({field_prefix: "user"})
-				end
+    describe "validating"
+      before_each
+        v = $(fixture("form")).jcheck()
+        v.validate(function(f) {})
+      end
 
-				it "should get prefixed field"
-					v.field("name").label().should.eql "Your Name"
-				end
+      it "should don't be valid if has errors"
+        v.validate(function(f) {f.errors.add("text", "some")})
+        v.is_valid().should.be_false
+      end
 
-				it "should able to get raw field"
-					v.field(":text").label().should.eql "TextField"
-				end
+      it "should be valid if has no errors"
+        v.is_valid().should.be_true
+      end
+    end
 
-				it "should generate field name correctly"
-					v.field_name("something").should.eql "user[something]"
-					v.field_name("list[]").should.eql "user[list][]"
-					v.field_name("more[deept][things][here]").should.eql "user[more][deept][things][here]"
-					v.field_name(":raw_name").should.eql "raw_name"
-				end
+    describe "notifiers setup"
+      it "should user notification_dialog and tip_ballons as default notifiers"
+        v = $(fixture("form")).jcheck()
+        v.should.have 1, 'notifiers'
+        v.should.have 1, 'live_notifiers'
+        v.notifiers[0].should.be_an $.FormCheck.Notifiers.NotificationDialog
+        v.live_notifiers[0].should.be_an $.FormCheck.Notifiers.TipBalloons
+      end
 
-				it "should reverse field name correctly"
-					v.reverse_field_name("user[something]").should.eql "something"
-					v.reverse_field_name("user[list][]").should.eql "list[]"
-					v.reverse_field_name("user[more][deept][things][here]").should.eql "more[deept][things][here]"
-					v.reverse_field_name("raw_name").should.eql ":raw_name"
-				end
-			end
-			
-			describe "field values"
-				before
-					v = $(fixture("form_values")).jcheck()
-				end
-				
-				it "should get correct value for an text input"
-					v.field("text").value().should.eql "some text"
-				end
-				
-				it "should get correct value for an text area"
-					v.field("textarea").value().should.eql "more text"
-				end
-				
-				it "should get correct value for a checked checkbox"
-					v.field("checked_checkbox").value().should.eql "20"
-				end
-				
-				it "should get correct value for an unchecked checkbox"
-					v.field("unchecked_checkbox").value().should.eql ""
-				end
-				
-				it "should get correct value for a list of checkboxes"
-					v.field("letters[]").value().should.eql ["a", "b"]
-				end
-				
-				it "should get correct value for a radio"
-					v.field("radios").value().should.eql "z"
-				end
-				
-				it "should get correct value for a select"
-					v.field("simple_select").value().should.eql "cc"
-				end
-				
-				it "should get correct value for a multiple select"
-					v.field("multiple_select").value().should.eql ["2", "4", "5"]
-				end
-			end
-			
-			describe "auto-parse"
-				describe "parsing validation strings"
-					it "should create the object with validations"
-						v = $("<form></form>").jcheck()
-						v.parse_validation_string("presence: true, format: /\\d+/").should.eql {presence: true, format: /\d+/}
-					end
-					
-					it "should return log error and return blank object if string is valid"
-						stub(console, 'error')
-						v = $("<form></form>").jcheck()
-						console.should.receive('error').with_args(an_instance_of(String))
-						v.parse_validation_string("presence: namaste").should.eql {}
-					end
-				end
-				
-				it "should parse fields with data-validation attribute"
-					v = $(fixture("form_inline_validations")).jcheck()
-					
-					v.should.have_error_message_on("can't be blank", "text")
-					v.should.have_error_message_on("can't be blank", "email")
-					v.should.have_error_message_on("is invalid", "email")
-				end
-				
-				it "should not parse fields if auto_parse option is sent as false"
-					v = $(fixture("form_inline_validations")).jcheck({auto_parse: false})
-					v.is_valid()
-					v.errors.size().should.be 0
-				end
-			end
-		end
-	end
+      it "should be able to customize overall notifiers on creation"
+        v = $(fixture("form")).jcheck({notifiers: []})
+        v.should.have 0, 'notifiers'
+        v.should.have 1, 'live_notifiers'
+        v.live_notifiers[0].should.be_an $.FormCheck.Notifiers.TipBalloons
+      end
+
+      it "should be able to customize live notifiers on creation"
+        v = $(fixture("form")).jcheck({live_notifiers: []})
+        v.should.have 1, 'notifiers'
+        v.should.have 0, 'live_notifiers'
+        v.notifiers[0].should.be_an $.FormCheck.Notifiers.NotificationDialog
+      end
+
+      it "should be able to customize both kind notifiers at once"
+        v = $(fixture("form")).jcheck({notifiers: [], live_notifiers: []})
+        v.should.have 0, 'notifiers'
+        v.should.have 0, 'live_notifiers'
+      end
+
+      it "should be able to customize default overall notifiers"
+        jQuery.FormCheck.default_notifiers = []
+
+        v = $(fixture("form")).jcheck()
+        v.should.have 0, 'notifiers'
+
+        jQuery.FormCheck.default_notifiers = ["notification_dialog"]
+      end
+
+      it "should be able to customize default live notifiers"
+        jQuery.FormCheck.default_live_notifiers = []
+
+        v = $(fixture("form")).jcheck()
+        v.should.have 0, 'live_notifiers'
+
+        jQuery.FormCheck.default_live_notifiers = ["tip_balloons"]
+      end
+    end
+
+    describe "Field"
+      describe "labels"
+        before_each
+          v = $(fixture("form")).jcheck()
+        end
+
+        it "should get label if it's present"
+          v.field("text").label().should.eql "TextField"
+        end
+
+        it "should return field name if label is not present"
+          v.field("multiple_select").label().should.eql "multiple_select"
+        end
+
+        it "should get label for multiple fields"
+          v.field("checkboxes[]").label().should.eql "Check List"
+        end
+
+        it "should able to use custom label"
+          v.field("text").custom_label = "Custom Label"
+          v.field("text").label().should.eql "Custom Label"
+        end
+      end
+
+      describe "field_prefix"
+        before_each
+          v = $(fixture("form")).jcheck({field_prefix: "user"})
+        end
+
+        it "should get prefixed field"
+          v.field("name").label().should.eql "Your Name"
+        end
+
+        it "should able to get raw field"
+          v.field(":text").label().should.eql "TextField"
+        end
+
+        it "should generate field name correctly"
+          v.field_name("something").should.eql "user[something]"
+          v.field_name("list[]").should.eql "user[list][]"
+          v.field_name("more[deept][things][here]").should.eql "user[more][deept][things][here]"
+          v.field_name(":raw_name").should.eql "raw_name"
+        end
+
+        it "should reverse field name correctly"
+          v.reverse_field_name("user[something]").should.eql "something"
+          v.reverse_field_name("user[list][]").should.eql "list[]"
+          v.reverse_field_name("user[more][deept][things][here]").should.eql "more[deept][things][here]"
+          v.reverse_field_name("raw_name").should.eql ":raw_name"
+        end
+      end
+
+      describe "field values"
+        before
+          v = $(fixture("form_values")).jcheck()
+        end
+
+        it "should get correct value for an text input"
+          v.field("text").value().should.eql "some text"
+        end
+
+        it "should get correct value for an text area"
+          v.field("textarea").value().should.eql "more text"
+        end
+
+        it "should get correct value for a checked checkbox"
+          v.field("checked_checkbox").value().should.eql "20"
+        end
+
+        it "should get correct value for an unchecked checkbox"
+          v.field("unchecked_checkbox").value().should.eql ""
+        end
+
+        it "should get correct value for a list of checkboxes"
+          v.field("letters[]").value().should.eql ["a", "b"]
+        end
+
+        it "should get correct value for a radio"
+          v.field("radios").value().should.eql "z"
+        end
+
+        it "should get correct value for a select"
+          v.field("simple_select").value().should.eql "cc"
+        end
+
+        it "should get correct value for a multiple select"
+          v.field("multiple_select").value().should.eql ["2", "4", "5"]
+        end
+      end
+
+      describe "auto-parse"
+        describe "parsing validation strings"
+          it "should create the object with validations"
+            v = $("<form></form>").jcheck()
+            v.parse_validation_string("presence: true, format: /\\d+/").should.eql {presence: true, format: /\d+/}
+          end
+
+          it "should return log error and return blank object if string is valid"
+            stub(console, 'error')
+            v = $("<form></form>").jcheck()
+            console.should.receive('error').with_args(an_instance_of(String))
+            v.parse_validation_string("presence: namaste").should.eql {}
+          end
+        end
+
+        it "should parse fields with data-validation attribute"
+          v = $(fixture("form_inline_validations")).jcheck()
+
+          v.should.have_error_message_on("can't be blank", "text")
+          v.should.have_error_message_on("can't be blank", "email")
+          v.should.have_error_message_on("is invalid", "email")
+        end
+
+        it "should not parse fields if auto_parse option is sent as false"
+          v = $(fixture("form_inline_validations")).jcheck({auto_parse: false})
+          v.is_valid()
+          v.errors.size().should.be 0
+        end
+      end
+    end
+  end
 end
--- src/jcheck.form.coffee
+++ src/jcheck.form.coffee
@@ -1,11 +1,11 @@
 # Copyright (c) 2010 Wilker Lúcio <wilkerlucio@gmail.com>
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,222 +13,227 @@
 # limitations under the License.
 
 (($) ->
-	class $.FormCheck
-		constructor: (@form, options) ->
-			@options = $.extend({
-				prevent_submit: true
-				field_prefix: null
-				notifiers: $.FormCheck.default_notifiers
-				live_notifiers: $.FormCheck.default_live_notifiers
-				language: $.FormCheck.default_locale
-				auto_parse: true
-			}, options || {})
-			
-			@field_cache = {}
-			@errors = new $.FormCheck.Errors(this)
-			@validations = []
-			@hook_events()
-			@setup_notifiers()
-			@parse_inline_validations() if @options.auto_parse
-		
-		parse_inline_validations: ->
-			@form.find(":input[data-validations]").each (index, element) =>
-				e = $(element)
-				field_name = @parse_field_name(e)
-				validations = @parse_validation_string(e.attr("data-validations"))
-				@validates(field_name, validations)
-		
-		parse_validation_string: (validation_string) ->
-			validations = {}
-			
-			try
-				validations = eval("({#{validation_string}})")
-			catch e
-				console.error("can't parse \"#{validation_string}\"")
-			
-			validations
-		
-		hook_events: ->
-			@form.submit (e) =>
-				unless @is_valid()
-					e.preventDefault() if @options.prevent_submit
-					notifier.notify() for notifier in @notifiers
-			
-			self = this
-			
-			@form.find(":input").each () ->
-				name = self.parse_field_name($(this))
-				
-				if name
-					field = self.field(name)
-					
-					if self.options.live_notifiers
-						((el) ->
-							n = name
-							f = field
-						
-							el.focus (e) ->
-								self.is_valid()
-								self.dispatch_live_notifiers("focus", n, e, f.live_notifiers)
-					
-							el.blur (e) ->
-								self.is_valid()
-								self.dispatch_live_notifiers("blur", n, e, f.live_notifiers)
-						)($(this))
-					
-		
-		dispatch_live_notifiers: (callback, attribute, event, notifiers) ->
-			current_notifiers = []
-			
-			for n in notifiers
-				if n == ":parent"
-					current_notifiers = current_notifiers.concat(@live_notifiers)
-				else
-					current_notifiers.push(@get_notifier(n))
-			
-			notifier[callback](attribute, event) for notifier in current_notifiers
-		
-		setup_notifiers: ->
-			for type in ["notifiers", "live_notifiers"]
-				this[type] = []
-				this[type] = @get_notifier(kind) for kind in @options[type] if @options[type]
-		
-		get_notifier: (notifier) ->
-			parameters = [this]
-			
-			if $.isArray(notifier)
-				parameters = parameters.concat(notifier.slice(1))
-				notifier = notifier[0]
-			
-			if $.isString(notifier)
-				notifier_class = $.FormCheck.find_notifier(notifier)
-					
-				creator = ->
-					notifier_class.apply(this, parameters)
-				
-				creator.prototype = notifier_class.prototype
-				
-				new creator()
-			else
-				notifier.form = this
-				notifier
-		
-		validate: (validator) ->
-			@validations.push(validator)
-		
-		field: (name) ->
-			@field_cache[name] ?= new $.FormCheck.Field(this, @field_name(name), name)
-			@field_cache[name]
-		
-		is_valid: ->
-			@errors.clear()
-			
-			for validation in @validations
-				validation(this)
-			
-			@errors.size() == 0
-		
-		parse_field_name: (input) ->
-			name = input.attr("name")
-			return null unless name
-			
-			@reverse_field_name(name)
-		
-		reverse_field_name: (name) ->
-			if @options.field_prefix
-				if matches = name.match(new RegExp("#{@options.field_prefix}\\[(.+?)\\](.*)"))
-					name = matches[1] + (matches[2] || "")
-				else
-					name = ":" + name
-			
-			name
-		
-		field_name: (name) ->
-			return matches[1] if matches = name.match(/^:(.+)/)
-			
-			if @options.field_prefix
-				subparts = ""
-				if matches = name.match(/(.+?)(\[.+)$/)
-					name = matches[1]
-					subparts = matches[2]
-				
-				"#{@options.field_prefix}[#{name}]#{subparts}"
-			else
-				name
-	
-	$.FormCheck.default_locale = "en"
-	$.FormCheck.default_notifiers = ["notification_dialog"]
-	$.FormCheck.default_live_notifiers = ["tip_balloons"]
-	
-	class $.FormCheck.Field
-		constructor: (form, name, attribute) ->
-			@form_checker = form
-			@field_name = name
-			@attribute = attribute
-			@live_notifiers = [":parent"]
-			@element = @form_checker.form.find(":input[name='#{name}']")
-			@custom_label = null
-			
-			if @form_checker.options.live_notifiers
-				for evt in @events_for_element()
-					@element[evt] (e) =>
-						@form_checker.is_valid()
-						@form_checker.dispatch_live_notifiers("notify", @attribute, e, @live_notifiers)
-		
-		events_for_element: ->
-			return [] if @element.length == 0
-			
-			if @element.attr("type") == "radio" or @element.attr("type") == "checkbox"
-				return ["change"]
-			if @element[0].tagName.toLowerCase() == "select"
-				return ["keyup", "change"]
-			
-			["keyup"]
-		
-		value: ->
-			if @element.attr("type") == "radio"
-				return @value_for_radio()
-			if @element.attr("type") == "checkbox"
-				return @value_for_checkbox()
-			
-			@value_for_text()
-		
-		value_for_text: ->
-			@element.val() || ""
-		
-		value_for_radio: ->
-			@element.filter(":checked").val() || ""
-		
-		value_for_checkbox: ->
-			if @element.length > 1
-				$.makeArray(@element.filter(":checked").map -> $(this).val())
-			else
-				if @element[0].checked then @element.val() else ""
-		
-		label: ->
-			unless @custom_label == null
-				if $.isFunction(@custom_label)
-				  return @custom_label.call(this)
-				else
-					return @custom_label
-			
-			field_id = @element.attr("id")
-			
-			if @element.length > 1
-				field_id = matches[1] if matches = field_id.match(/(.+)_.+$/)
-			
-			label_element = @form_checker.form.find("label[for='#{field_id}']")
-			
-			if label_element.length > 0
-				label_element.text()
-			else
-				@field_name
-	
-	# jquery hook
-	$.fn.jcheck = (options) ->
-		new $.FormCheck($(this), options || {})
-	
-	# auto_parse
-	$ ->
-		$("form[data-jcheck=true]").jcheck()
+  class $.FormCheck
+    constructor: (@form, options) ->
+      @options = $.extend({
+        prevent_submit: true
+        field_prefix:   null
+        notifiers:      $.FormCheck.default_notifiers
+        live_notifiers: $.FormCheck.default_live_notifiers
+        language:       $.FormCheck.default_locale
+        auto_parse:     true
+        enabled:        true
+      }, options || {})
+
+      @field_cache = {}
+      @errors = new $.FormCheck.Errors(this)
+      @validations = []
+      @hook_events()
+      @setup_notifiers()
+      @parse_inline_validations() if @options.auto_parse
+
+    parse_inline_validations: ->
+      @form.find(":input[data-validations]").each (index, element) =>
+        e = $(element)
+        field_name = @parse_field_name(e)
+        validations = @parse_validation_string(e.attr("data-validations"))
+        @validates(field_name, validations)
+
+    parse_validation_string: (validation_string) ->
+      validations = {}
+
+      try
+        validations = eval("({#{validation_string}})")
+      catch e
+        console.error("can't parse \"#{validation_string}\"")
+
+      validations
+
+    hook_events: ->
+      @form.submit (e) =>
+        return unless @options.enabled
+
+        unless @is_valid()
+          e.preventDefault() if @options.prevent_submit
+          notifier.notify() for notifier in @notifiers
+
+      self = this
+
+      @form.find(":input").each () ->
+        name = self.parse_field_name($(this))
+
+        if name
+          field = self.field(name)
+
+          if self.options.live_notifiers
+            ((el) ->
+              n = name
+              f = field
+
+              el.focus (e) ->
+                self.is_valid()
+                self.dispatch_live_notifiers("focus", n, e, f.live_notifiers)
+
+              el.blur (e) ->
+                self.is_valid()
+                self.dispatch_live_notifiers("blur", n, e, f.live_notifiers)
+            )($(this))
+
+    dispatch_live_notifiers: (callback, attribute, event, notifiers) ->
+      current_notifiers = []
+
+      for n in notifiers
+        if n == ":parent"
+          current_notifiers = current_notifiers.concat(@live_notifiers)
+        else
+          current_notifiers.push(@get_notifier(n))
+
+      notifier[callback](attribute, event) for notifier in current_notifiers
+
+    setup_notifiers: ->
+      for type in ["notifiers", "live_notifiers"]
+        this[type] = []
+        this[type] = @get_notifier(kind) for kind in @options[type] if @options[type]
+
+    get_notifier: (notifier) ->
+      parameters = [this]
+
+      if $.isArray(notifier)
+        parameters = parameters.concat(notifier.slice(1))
+        notifier = notifier[0]
+
+      if $.isString(notifier)
+        notifier_class = $.FormCheck.find_notifier(notifier)
+
+        creator = ->
+          notifier_class.apply(this, parameters)
+
+        creator.prototype = notifier_class.prototype
+
+        new creator()
+      else
+        notifier.form = this
+        notifier
+
+    validate: (validator) ->
+      @validations.push(validator)
+
+    field: (name) ->
+      @field_cache[name] ?= new $.FormCheck.Field(this, @field_name(name), name)
+      @field_cache[name]
+
+    is_valid: ->
+      @errors.clear()
+
+      for validation in @validations
+        validation(this)
+
+      @errors.size() == 0
+
+    parse_field_name: (input) ->
+      name = input.attr("name")
+      return null unless name
+
+      @reverse_field_name(name)
+
+    reverse_field_name: (name) ->
+      if @options.field_prefix
+        if matches = name.match(new RegExp("#{@options.field_prefix}\\[(.+?)\\](.*)"))
+          name = matches[1] + (matches[2] || "")
+        else
+          name = ":" + name
+
+      name
+
+    field_name: (name) ->
+      return matches[1] if matches = name.match(/^:(.+)/)
+
+      if @options.field_prefix
+        subparts = ""
+        if matches = name.match(/(.+?)(\[.+)$/)
+          name = matches[1]
+          subparts = matches[2]
+
+        "#{@options.field_prefix}[#{name}]#{subparts}"
+      else
+        name
+
+    disable: -> @options.enabled = false
+    enable: -> @options.enabled = true
+
+  $.FormCheck.default_locale = "en"
+  $.FormCheck.default_notifiers = ["notification_dialog"]
+  $.FormCheck.default_live_notifiers = ["tip_balloons"]
+
+  class $.FormCheck.Field
+    constructor: (form, name, attribute) ->
+      @form_checker = form
+      @field_name = name
+      @attribute = attribute
+      @live_notifiers = [":parent"]
+      @element = @form_checker.form.find(":input[name='#{name}']")
+      @custom_label = null
+
+      if @form_checker.options.live_notifiers
+        for evt in @events_for_element()
+          @element[evt] (e) =>
+            @form_checker.is_valid()
+            @form_checker.dispatch_live_notifiers("notify", @attribute, e, @live_notifiers)
+
+    events_for_element: ->
+      return [] if @element.length == 0
+
+      if @element.attr("type") == "radio" or @element.attr("type") == "checkbox"
+        return ["change"]
+      if @element[0].tagName.toLowerCase() == "select"
+        return ["keyup", "change"]
+
+      ["keyup"]
+
+    value: ->
+      if @element.attr("type") == "radio"
+        return @value_for_radio()
+      if @element.attr("type") == "checkbox"
+        return @value_for_checkbox()
+
+      @value_for_text()
+
+    value_for_text: ->
+      @element.val() || ""
+
+    value_for_radio: ->
+      @element.filter(":checked").val() || ""
+
+    value_for_checkbox: ->
+      if @element.length > 1
+        $.makeArray(@element.filter(":checked").map -> $(this).val())
+      else
+        if @element[0].checked then @element.val() else ""
+
+    label: ->
+      unless @custom_label == null
+        if $.isFunction(@custom_label)
+          return @custom_label.call(this)
+        else
+          return @custom_label
+
+      field_id = @element.attr("id")
+
+      if @element.length > 1
+        field_id = matches[1] if matches = field_id.match(/(.+)_.+$/)
+
+      label_element = @form_checker.form.find("label[for='#{field_id}']")
+
+      if label_element.length > 0
+        label_element.text()
+      else
+        @field_name
+
+  # jquery hook
+  $.fn.jcheck = (options) ->
+    new $.FormCheck($(this), options || {})
+
+  # auto_parse
+  $ ->
+    $("form[data-jcheck=true]").jcheck()
 )(jQuery)
